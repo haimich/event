d8fc64b3f4e34fbf744fa3806d7a9505be1ae71b
Thomas Reuter <thomas.reuter@1und1.de>
2015-10-20 15:44:02 +0000

    Cleanup

--- frontend/server.js
+++ frontend/server.js
@@ -2,9 +2,4 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('static'));
- 
- app.get('/', function(request, response) {
-    response.send("This would be some HTML");
-});
-
 app.listen(3000);
