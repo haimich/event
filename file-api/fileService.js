08f417519d3cb2286c83dc406d42978f290158c0
piranha87 <andreas.runk@1und1.de>
2015-10-21 12:08:05 +0000

    moved messaging to fileService

--- file-api/fileService.js
+++ file-api/fileService.js
@@ -1,5 +1,33 @@
 var fileRepo = require('./fileRepo');
+var amqp = require('amqplib');
+var when = require('when');
+var config = require('./config').readConfig().messageQueue;
 
 exports.createAttachment = function(fileModel, dbPool, success) {
-  fileRepo.createAttachment(fileModel, dbPool, success);
+  var id = fileRepo.createAttachment(fileModel, dbPool, 
+  	function (id) {
+  	  sendUploadFinished(id);
+  	  success();	
+  	});
+}
+
+function sendUploadFinished(attachementId){
+  var mqueue = config.host + config.port;
+  amqp.connect(mqueue).then(function(conn) {
+    return when(conn.createChannel().then(function(ch) {
+	  var queue = config.emitToQueue;
+      var ok = ch.assertExchange(queue, 'fanout', {durable: false})
+
+      // create JSON mqueue entry
+      var jsonMessage = JSON.parse('{}');
+      jsonMessage.attachementId = attachementId;
+      jsonMessage.uploadStatus = 'finished';
+
+      // emit message
+      return ok.then(function() {
+        ch.publish(queue, '', new Buffer(JSON.stringify(jsonMessage)));
+        return ch.close();
+      });
+    })).ensure(function() { conn.close(); });
+  }).then(null, console.warn);
 }
