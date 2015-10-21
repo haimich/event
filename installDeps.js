17f63ab3e7fd1a7daa20a90cf1107d3c3ab8a641
Michael Müller <haimich@gmx.net>
2015-10-21 11:46:59 +0000

    Added pm2 tool to manage node processes

---
+++ installDeps.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var exec = require('child_process').exec;
+
+fs.readFile('processes.json', 'utf8', function (error, data) {
+  var apps = (JSON.parse(data).apps);
+  
+  apps.forEach(function(app) {
+    exec('cd ' + app.cwd + '&& npm install', function (error, stdout, stderr) {
+      if (error !== null && error !== '') {
+        throw new Error('An error occured: ' + error);
+      }
+      console.log('Installed npm dependencies for ' + app.cwd);
+    });
+  });
+});
