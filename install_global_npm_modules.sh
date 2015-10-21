4b3b7dd6114eb0b433f44a0b6bcaad4c77988a72
Michael Müller <haimich@gmx.net>
2015-10-21 14:44:30 +0000

    Added install script for global npm modules

---
+++ install_global_npm_modules.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## pm2 process manager
+npm install -g pm2
+
+#auto complete
+pm2 completion install
+pm2 completion >> ~/.bashrc
+source ~/.bashrc
+
+#logrotate
+pm2 install pm2-logrotate
+pm2 set pm2-logrotate:retain 7
