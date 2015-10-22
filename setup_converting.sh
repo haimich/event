79f5799fdc2a35fd43156396dc0cc3f81777e747
Michael Müller <haimich@gmx.net>
2015-10-22 11:33:20 +0000

    setup script for video stuff

---
+++ setup_converting.sh
@@ -0,0 +1,10 @@
+# Add repo (Ubuntu)
+sudo add-apt-repository ppa:mc3man/trusty-media
+sudo apt-get update
+#sudo apt-get dist-upgrade => skipping, takes too much time
+
+# Add repo (Debian)
+# => see http://www.deb-multimedia.org/
+
+# ffmpeg
+sudo apt-get install ffmpeg
