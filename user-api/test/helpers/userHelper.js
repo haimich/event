4df87c13a23b6b6d58cb66327572cbea9f0a2aee
djuelg <djuelg@web.de>
2015-10-22 11:58:59 +0000

    Modified user-api server test

--- user-api/test/helpers/userHelper.js
+++ user-api/test/helpers/userHelper.js
@@ -1,3 +1,28 @@
 var request = require('request');
 
 var baseUrl = 'http://localhost:8080/event/api/user';
+var basePath = '/event/api/user?filter=';
+
+function getUser(userinfo, callback) {
+   request({
+    url: baseUrl,
+    path: basePath + userinfo,
+    method: 'GET'
+  }, function (error, response, body) {
+    if (error) { throw error; }
+    callback(response);
+  });
+}
+
+function getUserId(userid, callback) {
+   request({
+    url: baseUrl + '/' + userid,
+    method: 'GET'
+  }, function (error, response, body) {
+    if (error) { throw error; }
+    callback(response);
+  });
+}
+
+exports.getUser = getUser;
+exports.getUserId = getUserId;
--- user-api/test/serverTest.js
+++ user-api/test/serverTest.js
@@ -1,4 +1,33 @@
 var should = require('chai').should();
 var helper = require('./helpers/userHelper');
 var status = require('http-status');
-                                                                                                                                                                                      
+
+var userid = 1;
+var username = 'eventman';
+
+describe('GET /user', function() {
+  it('should return a user by username, firstname and name', function (done) {
+
+    helper.getUser(username, function(response) {
+      response.statusCode.should.equal(status.OK);
+      response.body.should.exist;
+      response.body.should.contain(username);
+      done();
+    });
+    
+  });
+});
+
+describe('GET /user/{id}', function() {
+  it('should return a user by id', function (done) {
+
+    helper.getUserId(userid, function(response) {
+      response.statusCode.should.equal(status.OK);
+      response.body.should.exist;
+      response.body.should.contain(username);
+      done();
+    });
+
+  });
+});
+                                                                                                                                                                               
