9707559cbc781501c6815dafd996440f90420a4a
Thomas Reuter <thomas.reuter@1und1.de>
2015-10-22 09:07:36 +0000

    Rename table

--- file-api/src/fileRepo.js
+++ file-api/src/fileRepo.js
@@ -2,7 +2,7 @@ var mysql = require('./mysql');
 
 exports.createAttachment = function (fileModel, dbPool, callback) {
  	dbPool.query(
-		"INSERT INTO attachment (url, mime_type, created_at, modified_at) VALUES (:url, :mime_type, :created_at, :modified_at)",
+		"INSERT INTO file (url, mime_type, created_at, modified_at) VALUES (:url, :mime_type, :created_at, :modified_at)",
 		fileModel
 	, function(err, rows) {
     callback(err, rows.insertId);
