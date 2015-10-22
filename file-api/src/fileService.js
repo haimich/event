c3e9e77317326234077b9b66e83022454c405658
piranha87 <andreas.runk@1und1.de>
2015-10-22 10:26:53 +0000

    removed moved function sendUploadFinished

--- file-api/src/fileService.js
+++ file-api/src/fileService.js
@@ -9,32 +9,10 @@ exports.createAttachment = function(fileModel, dbPool, callback) {
       callback(err, null);
       return;
     }
-    sendUploadFinished(id);
     callback(null, id);	
   });
 }
 
 exports.convertFile = function(fileId, dbPool) {
   console.log('Converting file with id ' + fileId);
-}
-
-function sendUploadFinished(attachementId){
-  var mqueue = config.host + config.port;
-  amqp.connect(mqueue).then(function(conn) {
-    return when(conn.createChannel().then(function(ch) {
-	  var queue = config.emitToQueue;
-      var ok = ch.assertExchange(queue, 'fanout', {durable: false})
-
-      // create JSON mqueue entry
-      var jsonMessage = JSON.parse('{}');
-      jsonMessage.attachementId = attachementId;
-      jsonMessage.uploadStatus = 'finished';
-
-      // emit message
-      return ok.then(function() {
-        ch.publish(queue, '', new Buffer(JSON.stringify(jsonMessage)));
-        return ch.close();
-      });
-    })).ensure(function() { conn.close(); });
-  }).then(null, console.warn);
 }
