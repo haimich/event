1be1f19b30d46d845c7ee8a7766f5a50fe338d85
Michael Müller <michael.mueller@1und1.de>
2015-10-30 17:51:06 +0100

    part 4

---
+++ session-api/src/helper/mysql.js
@@ -0,0 +1,26 @@
+var mysql  = require('mysql');
+
+exports.createPool = function(config){
+	var pool = mysql.createPool({
+		connectionLimit: config.database.connectionLimit,
+		host:            config.database.host,
+		user:            config.database.user,
+		password:        config.database.password,
+		database:        config.database.database,
+		debug:           config.database.debug
+	});
+  
+	// change query format from "?" to ":variable" syntax	
+	pool.config.connectionConfig.queryFormat = function (query, values) {
+		if (!values) return query;
+		return query.replace(/\:(\w+)/g, function (txt, key) {
+			if (values.hasOwnProperty(key)) {
+				return this.escape(values[key]);
+			}
+			return txt;
+		}.bind(this));
+	};
+
+	return pool;
+} 
+
