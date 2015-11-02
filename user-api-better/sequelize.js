71194516e60d5f00b43288bcbb29e1a3ed70e7ad
Michael Müller <michael.mueller@1und1.de>
2015-11-02 19:52:22 +0100

    added prototype for orm

---
+++ user-api-better/sequelize.js
@@ -0,0 +1,31 @@
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize('event', 'event', 'event', {
+  host: 'localhost',
+  dialect: 'sqlite',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+
+  // SQLite only
+  storage: 'app.sqlite'
+});
+
+var User = sequelize.define('User', {
+  username: Sequelize.STRING,
+  birthday: Sequelize.DATE
+});
+
+sequelize.sync().then(function() {
+  return User.create({
+    username: 'janedoe',
+    birthday: new Date(1980, 6, 20)
+  });
+}).then(function(jane) {
+  console.log(jane.get({
+    plain: true
+  }))
+});
