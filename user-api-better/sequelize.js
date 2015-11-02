e0c6b38b388447555c47d991187eb1fcc4d8d1b7
Michael Müller <haimich@gmx.net>
2015-11-02 23:10:45 +0100

    making progress

--- user-api-better/sequelize.js
+++ user-api-better/sequelize.js
@@ -2,30 +2,53 @@ var Sequelize = require('sequelize');
 
 var sequelize = new Sequelize('event', 'event', 'event', {
   host: 'localhost',
-  dialect: 'sqlite',
+  dialect: 'mysql',
 
   pool: {
     max: 5,
     min: 0,
     idle: 10000
   },
-
-  // SQLite only
-  storage: 'app.sqlite'
 });
 
-var User = sequelize.define('User', {
+var User = sequelize.define('user', {
+  firstname: Sequelize.STRING,
+  name: Sequelize.STRING,
   username: Sequelize.STRING,
-  birthday: Sequelize.DATE
+  externalId: {
+    type: Sequelize.INTEGER,
+    field: 'external_id'
+  },
+  email: {
+    type: Sequelize.STRING,
+    validate: { isEmail: true }
+  },
+  createdAt: {
+    type: Sequelize.DATE,
+    field: 'created_at',
+    defaultValue: Sequelize.NOW
+  },
+  modifiedAt: {
+    type: Sequelize.DATE,
+    field: 'modified_at',
+    defaultValue: Sequelize.NOW
+  }
+}, {
+  freezeTableName: true, // Model tableName will be the same as the model name
+  updatedAt: 'modifiedAt'
 });
 
-sequelize.sync().then(function() {
-  return User.create({
-    username: 'janedoe',
-    birthday: new Date(1980, 6, 20)
-  });
-}).then(function(jane) {
-  console.log(jane.get({
-    plain: true
-  }))
+User.findById(1).then(function(user) {
+  console.log(user.dataValues);
 });
+
+// sequelize.sync().then(function() {
+//   return User.get({
+//     username: 'janedoe',
+//     birthday: new Date(1980, 6, 20)
+//   });
+// }).then(function(jane) {
+//   console.log(jane.get({
+//     plain: true
+//   }))
+// });
