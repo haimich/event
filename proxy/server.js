bc50826a5cacd9e1df99baa75c9d084668517022
Michael Müller <michael.mueller@1und1.de>
2015-12-11 22:47:17 +0100

    more renaming

--- proxy/server.js
+++ proxy/server.js
@@ -15,8 +15,8 @@ var proxy = require('redbird')({port: ports.proxy});
 
 proxy.register(config.hostname + '/',                  'http://' + config.hostname + ':' + ports['proxy'] + '/event');
 proxy.register(config.hostname + '/event',             'http://localhost:' + ports['frontend']);
-proxy.register(config.hostname + '/event/api/users',   'http://localhost:' + ports['user-api']    + '/users');
-proxy.register(config.hostname + '/event/api/session', 'http://localhost:' + ports['session-api'] + '/session');
-proxy.register(config.hostname + '/event/api/file',    'http://localhost:' + ports['file-api']    + '/file');
+proxy.register(config.hostname + '/event/api/users',   'http://localhost:' + ports['users-api']    + '/users');
+proxy.register(config.hostname + '/event/api/sessions', 'http://localhost:' + ports['sessions-api'] + '/session');
+proxy.register(config.hostname + '/event/api/files',    'http://localhost:' + ports['files-api']    + '/file');
 proxy.register(config.hostname + '/event/apidocs',     'http://localhost:' + ports['apidocs']     + '/apidocs');
 proxy.register(config.hostname + '/event/api/share',   'http://localhost:' + ports['share-api']   + '/share');
