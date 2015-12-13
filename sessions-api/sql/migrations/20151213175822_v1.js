ab8e8678aabe4e12be211701143204d52b292f7d
Michael Müller <michael.mueller@1und1.de>
2015-12-13 19:00:25 +0100

    fixed down path

--- sessions-api/sql/migrations/20151213175822_v1.js
+++ sessions-api/sql/migrations/20151213175822_v1.js
@@ -15,31 +15,40 @@ exports.up = (knex, Promise) => {
       table.index(['id'], 'index_id');
     })
   ]).then(() => {
-    return knex.schema.createTable('sessions', (table) => {
-      table.bigIncrements('id').primary().unsigned();
-      table.string('title');
-      table.string('description');
+    return Promise.all([
+      knex.schema.createTable('sessions', (table) => {
+        table.bigIncrements('id').primary().unsigned();
+        table.string('title');
+        table.string('description');
+        
+        table.date('date');
+        table.time('start_time');
+        table.bigInteger('duration').unsigned();
+        table.bigInteger('speaker_id').unsigned();
+        
+        table.bigInteger('session_type_id').unsigned().defaultTo(0).references('id').inTable('session_type');
+        table.bigInteger('session_state_id').unsigned().defaultTo(0).references('id').inTable('session_state');
+        
+        table.dateTime('created_at');
+        table.dateTime('modified_at');
+        
+        table.index(['id'], 'index_id');
+      }),
       
-      table.date('date');
-      table.time('start_time');
-      table.bigInteger('duration').unsigned();
-      table.bigInteger('speaker_id').unsigned();
-      
-      table.bigInteger('session_type_id').unsigned().defaultTo(0).references('id').inTable('session_type');
-      table.bigInteger('session_state_id').unsigned().defaultTo(0).references('id').inTable('session_state');
-      
-      table.dateTime('created_at');
-      table.dateTime('modified_at');
-      
-      table.index(['id'], 'index_id');
-    });
+      knex('session_type').insert({ name: 'presentation' }),
+      knex('session_state').insert({ name: 'in progress' }),
+      knex('session_state').insert({ name: 'published' }),
+      knex('session_state').insert({ name: 'deleted' })
+    ]);
   });
 };
 
 exports.down = (knex, Promise) => {
-  return Promise.all([
-    knex.schema.dropTable('session_type'),
-    knex.schema.dropTable('session_state'),
-    knex.schema.dropTable('sessions')
-  ]);
+  return knex.schema.dropTable('sessions')
+    .then(() => {
+      return knex.schema.dropTable('session_type');
+    })
+    .then(() => {
+      return knex.schema.dropTable('session_state');
+    });
 };
