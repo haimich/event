e63e8e810596858d40467e90cec72b036903a348
Michael Müller <michael.mueller@1und1.de>
2015-12-18 16:56:03 +0100

    improved logging output

--- proxy/src/app.js
+++ proxy/src/app.js
@@ -7,12 +7,19 @@ let config = configHelper.loadConfig(args.config);
 let ports = configHelper.loadConfig(args.ports);
 let hostname = config.hostname;
 
-let proxy = require('redbird')({ port: ports['proxy'] });
+let proxy = require('redbird')({ port: ports['proxy'], bunyan: false });
 
-proxy.register(hostname + '/',                   `http://${hostname}:${ports['proxy']}/event`);
-proxy.register(hostname + '/event',              `http://${hostname}:${ports['frontend']}`);
-proxy.register(hostname + '/event/api/users',    `http://${hostname}:${ports['users-api']}/users`);
-proxy.register(hostname + '/event/api/sessions', `http://${hostname}:${ports['sessions-api']}/sessions`);
-proxy.register(hostname + '/event/api/files',    `http://${hostname}:${ports['files-api']}/files`);
-proxy.register(hostname + '/event/apidocs',      `http://${hostname}:${ports['apidocs']}/apidocs`);
-proxy.register(hostname + '/event/api/share',    `http://${hostname}:${ports['share-api']}/share`);
+console.log('Setting up routes:');
+
+register(hostname + '/',                   `http://${hostname}:${ports['proxy']}/event`);
+register(hostname + '/event',              `http://${hostname}:${ports['frontend']}`);
+register(hostname + '/event/api/users',    `http://${hostname}:${ports['users-api']}/users`);
+register(hostname + '/event/api/sessions', `http://${hostname}:${ports['sessions-api']}/sessions`);
+register(hostname + '/event/api/files',    `http://${hostname}:${ports['files-api']}/files`);
+register(hostname + '/event/apidocs',      `http://${hostname}:${ports['apidocs']}/apidocs`);
+register(hostname + '/event/api/share',    `http://${hostname}:${ports['share-api']}/share`);
+
+function register(from, to) {
+  console.log(from, ' -> ', to);
+  proxy.register(from, to);
+}
