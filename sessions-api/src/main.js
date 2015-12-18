ea488357407f94e4b15dd7b03c808743c939a9ad
Michael Müller <michael.mueller@1und1.de>
2015-12-18 18:01:33 +0100

    externalized routes

--- sessions-api/src/main.js
+++ sessions-api/src/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let configHelper = require('./helpers/config');
+let convertMessageConsumer = require('./services/convertMessageConsumer');
 
 let args = require('./helpers/arguments').parse();
 let config = configHelper.loadConfig(args.config);
@@ -9,118 +10,21 @@ let port = configHelper.loadKeyFromConfig(args.ports, 'sessions-api');
 let dbHelper = require('./helpers/db');
 dbHelper.initialize(config.knex);
 
-let Session = require('./models/sessionModel');
-let sessionService = require('./services/sessionService');
-let convertMessageConsumer = require('./services/convertMessageConsumer');
-let request = require('request-promise');
-
 let express = require('express');
-let status = require('http-status');
 let bodyParser = require('body-parser');
 let morgan = require('morgan');
+
+let routes = require('./routes/index');
+routes.initialize(config);
 
 let app = express();
 app.use(bodyParser.json());
 app.use(morgan('combined'));
 
-let baseUrl = 'http://localhost:8080/event/api';
+app.use('/', routes.router);
 
 // start listening for converted files messages
 convertMessageConsumer.listen(config);
-
-/**
- * Get all sessions.
- */
-app.get('/sessions', (request, response) => {
-  sessionService.getSessions()
-    .then((sessions) => {
-      response.json(sessions);
-    })
-    .catch((err) => {
-      return response.status(status.INTERNAL_SERVER_ERROR).json({ error: err });
-    });
-});
-
-app.get('/sessions/foobla', (request, response) => {
-  sessionService.deleteSessionFileByFileId(12).then((sf) => response.status(200).json(sf));
-});
-
-/**
- * Get a session by id.
- */
-app.get('/sessions/:id', (request, response) => {
-  let sessionId = request.params.id;
-
-  if (isNaN(sessionId) === true) {
-    return response.status(status.PRECONDITION_FAILED).json({ error: 'No session id given' });
-  }
-
-  sessionService.getSessionById(sessionId)
-    .then((session) => {
-      if (session === undefined) {
-        response.status(status.NOT_FOUND).json({ error: `Session with id ${sessionId} not found` });
-      } else {
-        response.json(session);
-      }
-    })
-    .catch((err) => {
-      return response.status(status.INTERNAL_SERVER_ERROR).json({ error: err });
-    });
-});
-
-/**
- * Create a session.
- */
-app.put('/sessions', (request, response) => {
-  let session = request.body;
-
-  if (session === undefined || session === null || Object.keys(session).length === 0) {
-    return response.status(status.PRECONDITION_FAILED).json({ error: 'No body given.'});
-  }
-
-  // create session
-  let sessionModel = new Session(session);
-  sessionService.createSession(sessionModel)
-    .then((sessionId) => {
-      if (sessionModel.files === null || sessionModel.files === undefined || sessionModel.files.length === 0) {
-        return response.status(status.CREATED).json({ id: sessionId });
-      } else {
-        // create session files
-        sessionService.createSessionFiles(sessionId, sessionModel.files)
-          .then(() => {
-            // start converting
-            console.log('TODO: startConvertProcess');
-            return response.status(status.CREATED).json({ id: sessionId });
-            // startConvertProcess(sessionModel.files)
-            //   .then(() => {
-            //     return response.status(status.CREATED).json({ id: sessionId });
-            //   })
-            //   .catch((err) => {
-            //     return response.status(status.INTERNAL_SERVER_ERROR).json({ error: err });
-            // });
-          })
-          .catch((err) => {
-            return response.status(status.INTERNAL_SERVER_ERROR).json({ error: err });
-          })
-      }
-    })
-    .catch((err) => {
-      return response.status(status.INTERNAL_SERVER_ERROR).json({ error: err });
-    })
-});
-
-function startConvertProcess(files) {
-  let promises = [];
-  
-  for (let file in files) {
-    promises.push(request({
-      url: baseUrl + '/files/' + file.id + '/convert',
-      method: 'PATCH'
-    }));
-  }
-  
-  return Promise.all(promises);
-}
 
 app.listen(port, () => {
   console.log('Server listening on port ' + port);
---
+++ sessions-api/src/routes/index.js
@@ -0,0 +1,112 @@
+'use strict';
+
+let Session = require('../models/sessionModel');
+let sessionService = require('../services/sessionService');
+let request = require('request-promise');
+
+let path = require('path');
+let express = require('express');
+let router = express.Router();
+let status = require('http-status');
+
+let baseUrl = 'http://localhost:8080/event/api';
+
+let config = null;
+
+function initialize(conf) {
+  config = conf;
+}
+
+/**
+ * Get all sessions.
+ */
+router.get('/sessions', (request, response) => {
+  sessionService.getSessions()
+    .then((sessions) => {
+      response.json(sessions);
+    })
+    .catch((err) => {
+      return response.status(status.INTERNAL_SERVER_ERROR).json({ error: err });
+    });
+});
+
+/**
+ * Get a session by id.
+ */
+router.get('/sessions/:id', (request, response) => {
+  let sessionId = request.params.id;
+
+  if (isNaN(sessionId) === true) {
+    return response.status(status.PRECONDITION_FAILED).json({ error: 'No session id given' });
+  }
+
+  sessionService.getSessionById(sessionId)
+    .then((session) => {
+      if (session === undefined) {
+        response.status(status.NOT_FOUND).json({ error: `Session with id ${sessionId} not found` });
+      } else {
+        response.json(session);
+      }
+    })
+    .catch((err) => {
+      return response.status(status.INTERNAL_SERVER_ERROR).json({ error: err });
+    });
+});
+
+/**
+ * Create a session.
+ */
+router.put('/sessions', (request, response) => {
+  let session = request.body;
+
+  if (session === undefined || session === null || Object.keys(session).length === 0) {
+    return response.status(status.PRECONDITION_FAILED).json({ error: 'No body given.'});
+  }
+
+  // create session
+  let sessionModel = new Session(session);
+  sessionService.createSession(sessionModel)
+    .then((sessionId) => {
+      if (sessionModel.files === null || sessionModel.files === undefined || sessionModel.files.length === 0) {
+        return response.status(status.CREATED).json({ id: sessionId });
+      } else {
+        // create session files
+        sessionService.createSessionFiles(sessionId, sessionModel.files)
+          .then(() => {
+            // start converting
+            console.log('TODO: startConvertProcess');
+            return response.status(status.CREATED).json({ id: sessionId });
+            // startConvertProcess(sessionModel.files)
+            //   .then(() => {
+            //     return response.status(status.CREATED).json({ id: sessionId });
+            //   })
+            //   .catch((err) => {
+            //     return response.status(status.INTERNAL_SERVER_ERROR).json({ error: err });
+            // });
+          })
+          .catch((err) => {
+            return response.status(status.INTERNAL_SERVER_ERROR).json({ error: err });
+          })
+      }
+    })
+    .catch((err) => {
+      return response.status(status.INTERNAL_SERVER_ERROR).json({ error: err });
+    })
+});
+
+function startConvertProcess(files) {
+  let promises = [];
+  
+  for (let file in files) {
+    promises.push(request({
+      url: baseUrl + '/files/' + file.id + '/convert',
+      method: 'PATCH'
+    }));
+  }
+  
+  return Promise.all(promises);
+}
+
+module.exports = {
+  initialize, router
+}
