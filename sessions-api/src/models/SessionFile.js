1c59af11e919e0eca734a237313fa2f7db806ffc
Michael Müller <michael.mueller@1und1.de>
2015-12-31 14:05:11 +0100

    made tests work

--- sessions-api/src/models/SessionFile.js
+++ sessions-api/src/models/SessionFile.js
@@ -2,10 +2,14 @@
 
 let moment = require('moment');
 
-module.exports = function SessionFile(session_id, file_id, type) {
-  this.session_id = session_id;
-  this.file_id = file_id;
-  this.type = type;
-  this.modified_timestamp = moment().valueOf();
-  this.state = null; //DEFAULT value
+class SessionFile {
+  constructor(session_id, file_id, type) {
+    this.session_id = session_id;
+    this.file_id = file_id;
+    this.type = type;
+    this.modified_timestamp = moment().valueOf();
+    this.state = null; //DEFAULT value
+  }
 }
+
+module.exports = SessionFile;
