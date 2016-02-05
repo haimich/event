48b86e5d8a2813d3623555a382fa1e0873caeaa9
bitionaire <johann.boehler@googlemail.com>
2016-02-05 10:30:49 +0100

    increased haimich happiness

--- backend/rabbitmq/src/main.js
+++ backend/rabbitmq/src/main.js
@@ -1,40 +1,42 @@
+'use strict';
+
 var fs = require('fs');
 var Docker = require('dockerode');
 
 var dockerSettings = {};
-if ( process.env.DOCKER_HOST ) {
+if (process.env.DOCKER_HOST) {
 
-    // Parsing DOCKER_HOST environment variable first to setup correct settings
-    var host = null;
-    var port = null;
+  // Parsing DOCKER_HOST environment variable first to setup correct settings
+  var host = null;
+  var port = null;
 
-    if (process.env.DOCKER_HOST.startsWith("tcp://")) {
-        var hostAndPort = process.env.DOCKER_HOST.substring(6).split(":");
+  if (process.env.DOCKER_HOST.startsWith('tcp://')) {
+    var hostAndPort = process.env.DOCKER_HOST.substring(6).split(':');
 
-        host = hostAndPort[0];
-        port = hostAndPort[1];
+    host = hostAndPort[0];
+    port = hostAndPort[1];
 
-        if (host == null || port == null) {
-            console.log("Failed to parse DOCKER_HOST: " + process.env.DOCKER_HOST);
-            process.exit(1);
-        }
-    } else {
-        console.log("Invalid DOCKER_HOST protocol: " + process.env.DOCKER_HOST);
-        process.exit(1);
+    if (host == null || port == null) {
+      console.log('Failed to parse DOCKER_HOST: ' + process.env.DOCKER_HOST);
+      process.exit(1);
     }
+  } else {
+    console.log('Invalid DOCKER_HOST protocol: ' + process.env.DOCKER_HOST);
+    process.exit(1);
+  }
 
-    dockerSettings = {
-        protocol: 'https',
-        host: host,
-        port: port,
-        ca: fs.readFileSync(process.env.DOCKER_CERT_PATH + '/ca.pem'),
-        cert: fs.readFileSync(process.env.DOCKER_CERT_PATH + '/cert.pem'),
-        key: fs.readFileSync(process.env.DOCKER_CERT_PATH + '/key.pem')
-    };
+  dockerSettings = {
+    protocol: 'https',
+    host: host,
+    port: port,
+    ca: fs.readFileSync(process.env.DOCKER_CERT_PATH + '/ca.pem'),
+    cert: fs.readFileSync(process.env.DOCKER_CERT_PATH + '/cert.pem'),
+    key: fs.readFileSync(process.env.DOCKER_CERT_PATH + '/key.pem')
+  };
 } else {
-    dockerSettings = {
-        socketPath: '/var/run/docker.sock'
-    };
+  dockerSettings = {
+    socketPath: '/var/run/docker.sock'
+  };
 }
 
 var docker = new Docker(dockerSettings);
@@ -42,42 +44,46 @@ var docker = new Docker(dockerSettings);
 // Pulling latest rabbitmq image
 docker.pull('rabbitmq:management', function (err, stream) {
 
-    // Register callback on finish
-    docker.modem.followProgress(stream, onFinished);
+  if (err != null) {
+    console.log('Failed to pull RabbitMQ image ' + err)
+  }
 
-    function onFinished(err, output) {
-        if (err == null) {
+  // Register callback on finish
+  docker.modem.followProgress(stream, onFinished);
 
-            // Create container of rabbitmq on successful pull of image
-            docker.createContainer({
-                Image: 'rabbitmq:management',
-                Env: [
-                    'RABBITMQ_DEFAULT_USER=development',
-                    'RABBITMQ_DEFAULT_PASS=topsecret123'
-                ],
-                ExposedPorts: { '15672/tcp': {} },
-                Name: "event-rabbitmq",
-            }, function(err, container) {
-                if (err == null && container != null) {
+  function onFinished(err, output) {
+    if (err == null) {
 
-                    // Start container
-                    container.start({}, function(err, data) {
-                        if (err != null) {
-                            console.log("Failed to start container: " + err);
-                            process.exit(1);
-                        }
-                    });
+      // Create container of rabbitmq on successful pull of image
+      docker.createContainer({
+        Image: 'rabbitmq:management',
+        Env: [
+          'RABBITMQ_DEFAULT_USER=development',
+          'RABBITMQ_DEFAULT_PASS=topsecret123'
+        ],
+        ExposedPorts: {'15672/tcp': {}},
+        Name: 'event-rabbitmq',
+      }, function (err, container) {
+        if (err == null && container != null) {
 
-                } else {
-                    console.log("Failed to create container: " + err);
-                    process.exit(1);
-                }
-            });
+          // Start container
+          container.start({}, function (err, data) {
+            if (err != null) {
+              console.log('Failed to start container: ' + err);
+              process.exit(1);
+            }
+          });
 
         } else {
-            console.log("Failed to pull image: " + err);
-            process.exit(1);
+          console.log('Failed to create container: ' + err);
+          process.exit(1);
         }
+      });
+
+    } else {
+      console.log('Failed to pull image: ' + err);
+      process.exit(1);
     }
+  }
 });
 
